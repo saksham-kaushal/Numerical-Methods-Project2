fbeed7e72049d67ceb10612d35e9bb4fa565c5ab
Saksham Kaushal <sakshamk.710@gmail.com>
2021-03-01 23:43:44 +0530

    initial files provided.

---
+++ task1/orbit19.f
@@ -0,0 +1,203 @@
+      program follow
+      implicit none
+      integer NVAR,NMAX,KMAXX
+      PARAMETER(NVAR=4,NMAX=50,KMAXX=10000) !same values as in odeint
+      INTEGER kmax,kount,nok,nbad,i,j
+      REAL ys(NVAR),xp(KMAXX),yp(NMAX,KMAXX),dxsav,x1,x2,ej,om,v0,a,q
+      EXTERNAL derivs,rkqs
+      COMMON /path/ kmax,kount,dxsav,xp,yp
+      COMMON /params/ om,v0,a,q
+c
+c     parameters of the potential
+c
+      om = 1.
+      v0 = 1.
+      a=0.03
+      q=0.8
+c
+c     initial conditions
+c
+      x1 = 0.
+      x2 = ::
+      ys(1) = ::                !x
+      ys(2) = ::                !vx
+      ys(3) = ::                !y
+      ys(4) = ::                !vy
+c
+      call odeint(ys,nvar,x1,x2,::,1.e-8,0.0,nok,nbad,derivs,rkqs)
+c
+      do 10 i=1,kmax
+      ej = 0.5*(yp(2,i)**2+yp(4,i)**2)-0.5*om**2*(yp(1,i)**2+yp(3,i)**2)
+     #      + 0.5*v0**2*alog(a**2+yp(1,i)**2+(yp(3,i)/q)**2)
+         write(9,33) xp(i), (yp(j,i),j=1,NVAR), ej
+         if(xp(i).eq.x2) goto 99
+ 10   continue
+ 99   continue
+ 33   format(5f10.4,f13.8)
+      end
+c
+      subroutine derivs(x,y,dydx)
+      implicit none
+      integer NVAR
+      PARAMETER(NVAR=4)         !same values as in main program
+      real x,y(NVAR),dydx(NVAR),om,v0,a,q
+      COMMON /params/ om,v0,a,q
+c
+      dydx(1) = y(2)
+      dydx(2) = ::
+      dydx(3) = y(4)
+      dydx(4) = ::
+      return
+      end
+c
+      SUBROUTINE odeint(ystart,nvar,x1,x2,eps,h1,hmin,nok,nbad,derivs,
+     *rkqs)
+      INTEGER nbad,nok,nvar,KMAXX,MAXSTP,NMAX
+      REAL eps,h1,hmin,x1,x2,ystart(nvar),TINY
+      EXTERNAL derivs,rkqs
+      PARAMETER (MAXSTP=1000000,NMAX=50,KMAXX=10000,TINY=1.e-30)
+      INTEGER i,kmax,kount,nstp
+      REAL dxsav,h,hdid,hnext,x,xsav,dydx(NMAX),xp(KMAXX),y(NMAX),
+     *yp(NMAX,KMAXX),yscal(NMAX)
+      COMMON /path/ kmax,kount,dxsav,xp,yp
+      kmax = KMAXX
+      dxsav = (x2-x1)/float(kmax)
+      x=x1
+      h=sign(h1,x2-x1)
+      nok=0
+      nbad=0
+      kount=0
+      do 11 i=1,nvar
+        y(i)=ystart(i)
+11    continue
+      if (kmax.gt.0) xsav=x-2.*dxsav
+      do 16 nstp=1,MAXSTP
+        call derivs(x,y,dydx)
+        do 12 i=1,nvar
+          yscal(i)=abs(y(i))+abs(h*dydx(i))+TINY
+12      continue
+        if(kmax.gt.0)then
+          if(abs(x-xsav).gt.abs(dxsav)) then
+            if(kount.lt.kmax-1)then
+              kount=kount+1
+              xp(kount)=x
+              do 13 i=1,nvar
+                yp(i,kount)=y(i)
+13            continue
+              xsav=x
+            endif
+          endif
+        endif
+        if((x+h-x2)*(x+h-x1).gt.0.) h=x2-x
+        call rkqs(y,dydx,nvar,x,h,eps,yscal,hdid,hnext,derivs)
+        if(hdid.eq.h)then
+          nok=nok+1
+        else
+          nbad=nbad+1
+        endif
+        if((x-x2)*(x2-x1).ge.0.)then
+          do 14 i=1,nvar
+            ystart(i)=y(i)
+14        continue
+          if(kmax.ne.0)then
+            kount=kount+1
+            xp(kount)=x
+            do 15 i=1,nvar
+              yp(i,kount)=y(i)
+15          continue
+          endif
+          return
+        endif
+        if(abs(hnext).lt.hmin) stop
+     *'stepsize smaller than minimum in odeint'
+        h=hnext
+16    continue
+      stop 'too many steps in odeint'
+      return
+      END
+c
+      SUBROUTINE rkqs(y,dydx,n,x,htry,eps,yscal,hdid,hnext,derivs)
+      INTEGER n,NMAX
+      REAL eps,hdid,hnext,htry,x,dydx(n),y(n),yscal(n)
+      EXTERNAL derivs
+      PARAMETER (NMAX=50)
+CU    USES derivs,rkck
+      INTEGER i
+      REAL errmax,h,htemp,xnew,yerr(NMAX),ytemp(NMAX),SAFETY,PGROW,
+     *PSHRNK,ERRCON
+      PARAMETER (SAFETY=0.9,PGROW=-.2,PSHRNK=-.25,ERRCON=1.89e-4)
+      h=htry
+1     call rkck(y,dydx,n,x,h,ytemp,yerr,derivs)
+      errmax=0.
+      do 11 i=1,n
+        errmax=max(errmax,abs(yerr(i)/yscal(i)))
+11    continue
+      errmax=errmax/eps
+      if(errmax.gt.1.)then
+        htemp=SAFETY*h*(errmax**PSHRNK)
+        h=sign(max(abs(htemp),0.1*abs(h)),h)
+        xnew=x+h
+        if(xnew.eq.x)stop 'stepsize underflow in rkqs'
+        goto 1
+      else
+        if(errmax.gt.ERRCON)then
+          hnext=SAFETY*h*(errmax**PGROW)
+        else
+          hnext=5.*h
+        endif
+        hdid=h
+        x=x+h
+        do 12 i=1,n
+          y(i)=ytemp(i)
+12      continue
+        return
+      endif
+      END
+c
+      SUBROUTINE rkck(y,dydx,n,x,h,yout,yerr,derivs)
+      INTEGER n,NMAX
+      REAL h,x,dydx(n),y(n),yerr(n),yout(n)
+      EXTERNAL derivs
+      PARAMETER (NMAX=50)
+CU    USES derivs
+      INTEGER i
+      REAL ak2(NMAX),ak3(NMAX),ak4(NMAX),ak5(NMAX),ak6(NMAX),
+     *ytemp(NMAX),A2,A3,A4,A5,A6,B21,B31,B32,B41,B42,B43,B51,B52,B53,
+     *B54,B61,B62,B63,B64,B65,C1,C3,C4,C6,DC1,DC3,DC4,DC5,DC6
+      PARAMETER (A2=.2,A3=.3,A4=.6,A5=1.,A6=.875,B21=.2,B31=3./40.,
+     *B32=9./40.,B41=.3,B42=-.9,B43=1.2,B51=-11./54.,B52=2.5,
+     *B53=-70./27.,B54=35./27.,B61=1631./55296.,B62=175./512.,
+     *B63=575./13824.,B64=44275./110592.,B65=253./4096.,C1=37./378.,
+     *C3=250./621.,C4=125./594.,C6=512./1771.,DC1=C1-2825./27648.,
+     *DC3=C3-18575./48384.,DC4=C4-13525./55296.,DC5=-277./14336.,
+     *DC6=C6-.25)
+      do 11 i=1,n
+        ytemp(i)=y(i)+B21*h*dydx(i)
+11    continue
+      call derivs(x+A2*h,ytemp,ak2)
+      do 12 i=1,n
+        ytemp(i)=y(i)+h*(B31*dydx(i)+B32*ak2(i))
+12    continue
+      call derivs(x+A3*h,ytemp,ak3)
+      do 13 i=1,n
+        ytemp(i)=y(i)+h*(B41*dydx(i)+B42*ak2(i)+B43*ak3(i))
+13    continue
+      call derivs(x+A4*h,ytemp,ak4)
+      do 14 i=1,n
+        ytemp(i)=y(i)+h*(B51*dydx(i)+B52*ak2(i)+B53*ak3(i)+B54*ak4(i))
+14    continue
+      call derivs(x+A5*h,ytemp,ak5)
+      do 15 i=1,n
+        ytemp(i)=y(i)+h*(B61*dydx(i)+B62*ak2(i)+B63*ak3(i)+B64*ak4(i)+
+     *B65*ak5(i))
+15    continue
+      call derivs(x+A6*h,ytemp,ak6)
+      do 16 i=1,n
+        yout(i)=y(i)+h*(C1*dydx(i)+C3*ak3(i)+C4*ak4(i)+C6*ak6(i))
+16    continue
+      do 17 i=1,n
+        yerr(i)=h*(DC1*dydx(i)+DC3*ak3(i)+DC4*ak4(i)+DC5*ak5(i)+DC6*
+     *ak6(i))
+17    continue
+      return
+      END
---
+++ task2/fourier19.f
@@ -0,0 +1,341 @@
+      program follow
+      implicit none
+      integer NVAR,NMAX,KMAXX
+      PARAMETER(NVAR=4,NMAX=50,KMAXX=8192) !same values as in odeint
+      INTEGER kmax,kount,nok,nbad,i,j,k
+      REAL y(NVAR),xp(KMAXX),yp(NMAX,KMAXX),dxsav,x1,x2,ej,om,v0,a,q
+      REAL step
+      REAL rw(KMAXX),prw(kmaxx),prwtot,rav
+      EXTERNAL derivs,rkqs
+      COMMON /path/ kmax,kount,dxsav,xp,yp
+      COMMON /params/ om,v0,a,q
+c
+c     parameters of the potential and the step
+c
+      om = 1.
+      v0 = 1.
+      a=0.03
+      q=0.8
+      step=2.e-2
+c
+c     initial conditions
+c
+      x1 = 0.
+      x2 = step
+      y(1) = ::                 !x
+      y(2) = ::                 !vx
+      y(3) = ::                 !y
+      y(4) = ::                 !vy
+c
+c     orbit calculation, including radius and radial velocity
+c
+      rav   = 0.
+c
+      do 5  i=1,kmaxx
+c
+         call odeint(y,nvar,x1,x2,::,1.e-8,0.0,nok,nbad,derivs,rkqs)
+c
+         ej = 0.5*(y(2)**2+y(4)**2)-0.5*om**2*(y(1)**2+y(3)**2)
+     #      + 0.5*v0**2*alog(a**2+y(1)**2+(y(3)/q)**2)
+         rw  (i) = sqrt(y(1)*y(1)+y(3)*y(3))
+         rav = rav + rw  (i)
+         x1=x2
+         x2=float(i+1)*step
+ 5    continue
+c
+      rav = rav/float(kmaxx) 
+      write(*,*) 'average radius: ', rav
+c
+      do 6 k=1,kmaxx
+         rw(k) = rw(k) - rav
+ 6    continue
+c
+      write(*,*) 'orbit calculated'
+c
+c     FFT and PSD calculation
+c
+      call realft(rw,kmaxx,1)
+c
+      prwtot = 0.
+      do 10 k=1,kmaxx/2
+         prw(k) = 2.*(rw(2*k-1)*rw(2*k-1) + rw(2*k)*rw(2*k))
+         prwtot = prwtot + prw(k)
+ 10   continue
+c
+      do 11 k=1,kmaxx/2
+         write(25,35) k, prw(k)/prwtot*100., alog10(prw(k)/prwtot)
+ 11   continue
+ 35   format(i5,f15.5,f8.3)
+      end
+c
+      subroutine derivs(x,y,dydx)
+      implicit none
+      integer NVAR
+      PARAMETER(NVAR=4)         !same values as in main program
+      real x,y(NVAR),dydx(NVAR),om,v0,a,q
+      COMMON /params/ om,v0,a,q
+c
+      dydx(1) = y(2)
+      dydx(2) = ::
+      dydx(3) = y(4)
+      dydx(4) = ::
+      return
+      end
+c
+      SUBROUTINE odeint(ystart,nvar,x1,x2,eps,h1,hmin,nok,nbad,derivs,
+     *rkqs)
+      INTEGER nbad,nok,nvar,KMAXX,MAXSTP,NMAX
+      REAL eps,h1,hmin,x1,x2,ystart(nvar),TINY
+      EXTERNAL derivs,rkqs
+      PARAMETER (MAXSTP=10000,NMAX=50,KMAXX=8192,TINY=1.e-30)
+      INTEGER i,kmax,kount,nstp
+      REAL dxsav,h,hdid,hnext,x,xsav,dydx(NMAX),xp(KMAXX),y(NMAX),
+     *yp(NMAX,KMAXX),yscal(NMAX)
+      COMMON /path/ kmax,kount,dxsav,xp,yp
+      kmax = KMAXX
+      dxsav = (x2-x1)/float(kmax)
+      x=x1
+      h=sign(h1,x2-x1)
+      nok=0
+      nbad=0
+      kount=0
+      do 11 i=1,nvar
+        y(i)=ystart(i)
+11    continue
+      if (kmax.gt.0) xsav=x-2.*dxsav
+      do 16 nstp=1,MAXSTP
+        call derivs(x,y,dydx)
+        do 12 i=1,nvar
+          yscal(i)=abs(y(i))+abs(h*dydx(i))+TINY
+12      continue
+        if(kmax.gt.0)then
+          if(abs(x-xsav).gt.abs(dxsav)) then
+            if(kount.lt.kmax-1)then
+              kount=kount+1
+              xp(kount)=x
+              do 13 i=1,nvar
+                yp(i,kount)=y(i)
+13            continue
+              xsav=x
+            endif
+          endif
+        endif
+        if((x+h-x2)*(x+h-x1).gt.0.) h=x2-x
+        call rkqs(y,dydx,nvar,x,h,eps,yscal,hdid,hnext,derivs)
+        if(hdid.eq.h)then
+          nok=nok+1
+        else
+          nbad=nbad+1
+        endif
+        if((x-x2)*(x2-x1).ge.0.)then
+          do 14 i=1,nvar
+            ystart(i)=y(i)
+14        continue
+          if(kmax.ne.0)then
+            kount=kount+1
+            xp(kount)=x
+            do 15 i=1,nvar
+              yp(i,kount)=y(i)
+15          continue
+          endif
+          return
+        endif
+        if(abs(hnext).lt.hmin) stop
+     *'stepsize smaller than minimum in odeint'
+        h=hnext
+16    continue
+      stop 'too many steps in odeint'
+      return
+      END
+c
+      SUBROUTINE rkqs(y,dydx,n,x,htry,eps,yscal,hdid,hnext,derivs)
+      INTEGER n,NMAX
+      REAL eps,hdid,hnext,htry,x,dydx(n),y(n),yscal(n)
+      EXTERNAL derivs
+      PARAMETER (NMAX=50)
+CU    USES derivs,rkck
+      INTEGER i
+      REAL errmax,h,htemp,xnew,yerr(NMAX),ytemp(NMAX),SAFETY,PGROW,
+     *PSHRNK,ERRCON
+      PARAMETER (SAFETY=0.9,PGROW=-.2,PSHRNK=-.25,ERRCON=1.89e-4)
+      h=htry
+1     call rkck(y,dydx,n,x,h,ytemp,yerr,derivs)
+      errmax=0.
+      do 11 i=1,n
+        errmax=max(errmax,abs(yerr(i)/yscal(i)))
+11    continue
+      errmax=errmax/eps
+      if(errmax.gt.1.)then
+        htemp=SAFETY*h*(errmax**PSHRNK)
+        h=sign(max(abs(htemp),0.1*abs(h)),h)
+        xnew=x+h
+        if(xnew.eq.x)stop 'stepsize underflow in rkqs'
+        goto 1
+      else
+        if(errmax.gt.ERRCON)then
+          hnext=SAFETY*h*(errmax**PGROW)
+        else
+          hnext=5.*h
+        endif
+        hdid=h
+        x=x+h
+        do 12 i=1,n
+          y(i)=ytemp(i)
+12      continue
+        return
+      endif
+      END
+c
+      SUBROUTINE rkck(y,dydx,n,x,h,yout,yerr,derivs)
+      INTEGER n,NMAX
+      REAL h,x,dydx(n),y(n),yerr(n),yout(n)
+      EXTERNAL derivs
+      PARAMETER (NMAX=50)
+CU    USES derivs
+      INTEGER i
+      REAL ak2(NMAX),ak3(NMAX),ak4(NMAX),ak5(NMAX),ak6(NMAX),
+     *ytemp(NMAX),A2,A3,A4,A5,A6,B21,B31,B32,B41,B42,B43,B51,B52,B53,
+     *B54,B61,B62,B63,B64,B65,C1,C3,C4,C6,DC1,DC3,DC4,DC5,DC6
+      PARAMETER (A2=.2,A3=.3,A4=.6,A5=1.,A6=.875,B21=.2,B31=3./40.,
+     *B32=9./40.,B41=.3,B42=-.9,B43=1.2,B51=-11./54.,B52=2.5,
+     *B53=-70./27.,B54=35./27.,B61=1631./55296.,B62=175./512.,
+     *B63=575./13824.,B64=44275./110592.,B65=253./4096.,C1=37./378.,
+     *C3=250./621.,C4=125./594.,C6=512./1771.,DC1=C1-2825./27648.,
+     *DC3=C3-18575./48384.,DC4=C4-13525./55296.,DC5=-277./14336.,
+     *DC6=C6-.25)
+      do 11 i=1,n
+        ytemp(i)=y(i)+B21*h*dydx(i)
+11    continue
+      call derivs(x+A2*h,ytemp,ak2)
+      do 12 i=1,n
+        ytemp(i)=y(i)+h*(B31*dydx(i)+B32*ak2(i))
+12    continue
+      call derivs(x+A3*h,ytemp,ak3)
+      do 13 i=1,n
+        ytemp(i)=y(i)+h*(B41*dydx(i)+B42*ak2(i)+B43*ak3(i))
+13    continue
+      call derivs(x+A4*h,ytemp,ak4)
+      do 14 i=1,n
+        ytemp(i)=y(i)+h*(B51*dydx(i)+B52*ak2(i)+B53*ak3(i)+B54*ak4(i))
+14    continue
+      call derivs(x+A5*h,ytemp,ak5)
+      do 15 i=1,n
+        ytemp(i)=y(i)+h*(B61*dydx(i)+B62*ak2(i)+B63*ak3(i)+B64*ak4(i)+
+     *B65*ak5(i))
+15    continue
+      call derivs(x+A6*h,ytemp,ak6)
+      do 16 i=1,n
+        yout(i)=y(i)+h*(C1*dydx(i)+C3*ak3(i)+C4*ak4(i)+C6*ak6(i))
+16    continue
+      do 17 i=1,n
+        yerr(i)=h*(DC1*dydx(i)+DC3*ak3(i)+DC4*ak4(i)+DC5*ak5(i)+DC6*
+     *ak6(i))
+17    continue
+      return
+      END
+c
+      SUBROUTINE four1(data,nn,isign)
+      INTEGER isign,nn
+      REAL data(2*nn)
+      INTEGER i,istep,j,m,mmax,n
+      REAL tempi,tempr
+      DOUBLE PRECISION theta,wi,wpi,wpr,wr,wtemp
+      n=2*nn
+      j=1
+      do 11 i=1,n,2
+        if(j.gt.i)then
+          tempr=data(j)
+          tempi=data(j+1)
+          data(j)=data(i)
+          data(j+1)=data(i+1)
+          data(i)=tempr
+          data(i+1)=tempi
+        endif
+        m=n/2
+1       if ((m.ge.2).and.(j.gt.m)) then
+          j=j-m
+          m=m/2
+        goto 1
+        endif
+        j=j+m
+11    continue
+      mmax=2
+2     if (n.gt.mmax) then
+        istep=2*mmax
+        theta=6.28318530717959d0/(isign*mmax)
+        wpr=-2.d0*sin(0.5d0*theta)**2
+        wpi=sin(theta)
+        wr=1.d0
+        wi=0.d0
+        do 13 m=1,mmax,2
+          do 12 i=m,n,istep
+            j=i+mmax
+            tempr=sngl(wr)*data(j)-sngl(wi)*data(j+1)
+            tempi=sngl(wr)*data(j+1)+sngl(wi)*data(j)
+            data(j)=data(i)-tempr
+            data(j+1)=data(i+1)-tempi
+            data(i)=data(i)+tempr
+            data(i+1)=data(i+1)+tempi
+12        continue
+          wtemp=wr
+          wr=wr*wpr-wi*wpi+wr
+          wi=wi*wpr+wtemp*wpi+wi
+13      continue
+        mmax=istep
+      goto 2
+      endif
+      return
+      END
+c
+      SUBROUTINE realft(data,n,isign)
+      INTEGER isign,n
+      REAL data(n)
+CU    USES four1
+      INTEGER i,i1,i2,i3,i4,n2p3
+      REAL c1,c2,h1i,h1r,h2i,h2r,wis,wrs
+      DOUBLE PRECISION theta,wi,wpi,wpr,wr,wtemp
+      theta=3.141592653589793d0/dble(n/2)
+      c1=0.5
+      if (isign.eq.1) then
+        c2=-0.5
+        call four1(data,n/2,+1)
+      else
+        c2=0.5
+        theta=-theta
+      endif
+      wpr=-2.0d0*sin(0.5d0*theta)**2
+      wpi=sin(theta)
+      wr=1.0d0+wpr
+      wi=wpi
+      n2p3=n+3
+      do 11 i=2,n/4
+        i1=2*i-1
+        i2=i1+1
+        i3=n2p3-i2
+        i4=i3+1
+        wrs=sngl(wr)
+        wis=sngl(wi)
+        h1r=c1*(data(i1)+data(i3))
+        h1i=c1*(data(i2)-data(i4))
+        h2r=-c2*(data(i2)+data(i4))
+        h2i=c2*(data(i1)-data(i3))
+        data(i1)=h1r+wrs*h2r-wis*h2i
+        data(i2)=h1i+wrs*h2i+wis*h2r
+        data(i3)=h1r-wrs*h2r+wis*h2i
+        data(i4)=-h1i+wrs*h2i+wis*h2r
+        wtemp=wr
+        wr=wr*wpr-wi*wpi+wr
+        wi=wi*wpr+wtemp*wpi+wi
+11    continue
+      if (isign.eq.1) then
+        h1r=data(1)
+        data(1)=h1r+data(2)
+        data(2)=h1r-data(2)
+      else
+        h1r=data(1)
+        data(1)=c1*(h1r+data(2))
+        data(2)=c1*(h1r-data(2))
+        call four1(data,n/2,-1)
+      endif
+      return
+      END
